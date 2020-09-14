6bb074558f29614d0d065cd6798371b34d1ed9ff
Lucas Samel <lucas.samel97@gmail.com>
2020-09-14 16:44:37 -0300

    Upload de Imagem

---
+++ SocialNetworkAPI/Controllers/ImagemController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetworkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagemController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        public ImagemController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string texto = " Web API - ImagemController em execução : " + DateTime.Now.ToLongTimeString();
+            texto += $"\n Ambiente :  {_environment.EnvironmentName}";
+            return texto;
+        }
+
+        [HttpPost("upload")]
+        public async Task<string> EnviaArquivo([FromForm] IFormFile arquivo)
+        {
+            if (arquivo.Length > 0)
+            {
+                try
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\imagens\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\imagens\\");
+                    }
+                    using (FileStream filestream = System.IO.File.Create(_environment.WebRootPath + "\\imagens\\" + arquivo.FileName))
+                    {
+                        await arquivo.CopyToAsync(filestream);
+                        filestream.Flush();
+                        return "\\imagens\\" + arquivo.FileName;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+            else
+            {
+                return "Ocorreu uma falha no envio do arquivo...";
+            }
+
+
+        }
+    }
+}
