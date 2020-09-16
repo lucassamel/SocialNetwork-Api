4d3cb3a6f569942e5c1c0fc0f4fcfaf24a98fc59
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-16 10:36:50 -0300

    return errors via response

--- SocialNetworkAPI/Controllers/AccountController.cs
+++ SocialNetworkAPI/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,31 +29,24 @@ namespace SocialNetworkAPI.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] Register model)
         {
-            if (ModelState.IsValid)
+            // Copia os dados do RegisterViewModel para o IdentityUser
+            var user = new IdentityUser
             {
-                // Copia os dados do RegisterViewModel para o IdentityUser
-                var user = new IdentityUser
-                {
-                    UserName = model.Email,
-                    Email = model.Email
-                };
-                // Armazena os dados do usuário na tabela AspNetUsers
-                var result = await userManager.CreateAsync(user, model.Password);
-                // Se o usuário foi criado com sucesso, faz o login do usuário
-                // usando o serviço SignInManager e redireciona para o Método Action Index
-                if (result.Succeeded)
-                {
-                    await signInManager.SignInAsync(user, isPersistent: false);
-                    return Ok();
-                }
-                // Se houver erros então inclui no ModelState
-                // que será exibido pela tag helper summary na validação
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
+                UserName = model.Email,
+                Email = model.Email
+            };
+            // Armazena os dados do usuário na tabela AspNetUsers
+            var result = await userManager.CreateAsync(user, model.Password);
+            // Se o usuário foi criado com sucesso, faz o login do usuário
+            // usando o serviço SignInManager e redireciona para o Método Action Index
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
             }
-            return Created(string.Empty, string.Empty);
+            // Se houver erros então inclui no ModelState
+            // que será exibido pela tag helper summary na validação
+            return StatusCode(StatusCodes.Status500InternalServerError, result.Errors);  
         }
 
         //Método que faz o Logout
