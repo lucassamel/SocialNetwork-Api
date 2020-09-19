ba35f00ccd685a0b70f9af1cdbbff63022b6e297
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-18 22:14:51 -0300

    add key in FileData

--- SocialNetworkBLL/Models/FileData.cs
+++ SocialNetworkBLL/Models/FileData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,7 +8,8 @@ namespace SocialNetworkBLL.Models
 {
     public class FileData
     {
-
+        [Key]
+        public int Id { get; set; }
         public string FileName { get; set; }
         public string FileSize { get; set; }
         public string ModifiedOn { get; set; }
