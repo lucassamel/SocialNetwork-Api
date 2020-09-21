848033de2f37fedf076140ba59a3994f8933c4df
Lucas Samel <lucas.samel97@gmail.com>
2020-09-21 16:00:49 -0300

    Cria a imagem junto com o post

--- SocialNetworkAPI/Controllers/PostController.cs
+++ SocialNetworkAPI/Controllers/PostController.cs
@@ -93,8 +93,9 @@ namespace SocialNetworkAPI.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Post>> PostPost(Post post, int id)
+        public async Task<ActionResult<Post>> PostPost(Post post, int id, IFormFile files)
         {
+            post.Imagem = await _ImageService.ImagemPost(files, post);
             post.PerfilId = id;
             post.DataPost = DateTime.Now;
             _context.Posts.Add(post);
